5859f61ec78feeacd463188024acb5f7a0511405
A252420 <A252420@LSBSA-GQV6HW3>
2024-01-11 12:42:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Sara";
+            int age = 5;
+            bool isTall = false;
+            double weight = 25.5;
+
+            Console.WriteLine("Hi , " + name + "  You're  " + age + " years old" + "  and your weight is " + weight);
+            Console.WriteLine("True or false, Are you tall? :  " + isTall);
+
+            //Freeaze my console
+            Console.ReadLine();
+        }
+    }
+}
